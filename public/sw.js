8e732aa5afaad2816ca4ccc4466303f8e8ba7072
Million72 <135624698+Million72@users.noreply.github.com>
2026-08-10 17:43:09 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'mt5-signal-pro-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
